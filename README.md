44c1cbd6b2fb8fb7cd189efc030c018cd0bfe564
nselvara <43971369+nselvara@users.noreply.github.com>
2021-12-13 22:24:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# HD44780-library
+
+<div id="top"></div>
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Don't forget to give the project a star!
+*** Thanks again! Now go create something AMAZING! :D
+-->
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <h3 align="center">Best-README-Template</h3>
+
+  <p align="center">
+    An awesome README template to jumpstart your projects!
+    <br />
+    <a href="https://github.com/othneildrew/Best-README-Template"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/othneildrew/Best-README-Template">View Demo</a>
+    ·
+    <a href="https://github.com/othneildrew/Best-README-Template/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/othneildrew/Best-README-Template/issues">Request Feature</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+A C-based library written for HD44780 based dot-matrix displays driven by Xilinx MicroBlaze softcore.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* [Xilinx SDK](https://www.xilinx.com/products/design-tools/ise-design-suite.html)
+* [Xilinx MicroBlaze](https://www.xilinx.com/products/design-tools/microblaze.html)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+### Prerequisites
+
+For a given processor change the GPIO manipulation functions according to your need.
+
+<!-- LICENSE -->
+## License
+
+Distributed under the Apache 2.0 License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
---
+++ lcd.h
@@ -0,0 +1,339 @@
+/*                                 Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+      "You" (or "Your") shall mean an individual or Legal Entity
+      exercising permissions granted by this License.
+
+      "Source" form shall mean the preferred form for making modifications,
+      including but not limited to software source code, documentation
+      source, and configuration files.
+
+      "Object" form shall mean any form resulting from mechanical
+      transformation or translation of a Source form, including but
+      not limited to compiled object code, generated documentation,
+      and conversions to other media types.
+
+      "Work" shall mean the work of authorship, whether in Source or
+      Object form, made available under the License, as indicated by a
+      copyright notice that is included in or attached to the work
+      (an example is provided in the Appendix below).
+
+      "Derivative Works" shall mean any work, whether in Source or Object
+      form, that is based on (or derived from) the Work and for which the
+      editorial revisions, annotations, elaborations, or other modifications
+      represent, as a whole, an original work of authorship. For the purposes
+      of this License, Derivative Works shall not include works that remain
+      separable from, or merely link (or bind by name) to the interfaces of,
+      the Work and Derivative Works thereof.
+
+      "Contribution" shall mean any work of authorship, including
+      the original version of the Work and any modifications or additions
+      to that Work or Derivative Works thereof, that is intentionally
+      submitted to Licensor for inclusion in the Work by the copyright owner
+      or by an individual or Legal Entity authorized to submit on behalf of
+      the copyright owner. For the purposes of this definition, "submitted"
+      means any form of electronic, verbal, or written communication sent
+      to the Licensor or its representatives, including but not limited to
+      communication on electronic mailing lists, source code control systems,
+      and issue tracking systems that are managed by, or on behalf of, the
+      Licensor for the purpose of discussing and improving the Work, but
+      excluding communication that is conspicuously marked or otherwise
+      designated in writing by the copyright owner as "Not a Contribution."
+
+      "Contributor" shall mean Licensor and any individual or Legal Entity
+      on behalf of whom a Contribution has been received by Licensor and
+      subsequently incorporated within the Work.
+
+   2. Grant of Copyright License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      copyright license to reproduce, prepare Derivative Works of,
+      publicly display, publicly perform, sublicense, and distribute the
+      Work and such Derivative Works in Source or Object form.
+
+   3. Grant of Patent License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      (except as stated in this section) patent license to make, have made,
+      use, offer to sell, sell, import, and otherwise transfer the Work,
+      where such license applies only to those patent claims licensable
+      by such Contributor that are necessarily infringed by their
+      Contribution(s) alone or by combination of their Contribution(s)
+      with the Work to which such Contribution(s) was submitted. If You
+      institute patent litigation against any entity (including a
+      cross-claim or counterclaim in a lawsuit) alleging that the Work
+      or a Contribution incorporated within the Work constitutes direct
+      or contributory patent infringement, then any patent licenses
+      granted to You under this License for that Work shall terminate
+      as of the date such litigation is filed.
+
+   4. Redistribution. You may reproduce and distribute copies of the
+      Work or Derivative Works thereof in any medium, with or without
+      modifications, and in Source or Object form, provided that You
+      meet the following conditions:
+
+      (a) You must give any other recipients of the Work or
+          Derivative Works a copy of this License; and
+
+      (b) You must cause any modified files to carry prominent notices
+          stating that You changed the files; and
+
+      (c) You must retain, in the Source form of any Derivative Works
+          that You distribute, all copyright, patent, trademark, and
+          attribution notices from the Source form of the Work,
+          excluding those notices that do not pertain to any part of
+          the Derivative Works; and
+
+      (d) If the Work includes a "NOTICE" text file as part of its
+          distribution, then any Derivative Works that You distribute must
+          include a readable copy of the attribution notices contained
+          within such NOTICE file, excluding those notices that do not
+          pertain to any part of the Derivative Works, in at least one
+          of the following places: within a NOTICE text file distributed
+          as part of the Derivative Works; within the Source form or
+          documentation, if provided along with the Derivative Works; or,
+          within a display generated by the Derivative Works, if and
+          wherever such third-party notices normally appear. The contents
+          of the NOTICE file are for informational purposes only and
+          do not modify the License. You may add Your own attribution
+          notices within Derivative Works that You distribute, alongside
+          or as an addendum to the NOTICE text from the Work, provided
+          that such additional attribution notices cannot be construed
+          as modifying the License.
+
+      You may add Your own copyright statement to Your modifications and
+      may provide additional or different license terms and conditions
+      for use, reproduction, or distribution of Your modifications, or
+      for any such Derivative Works as a whole, provided Your use,
+      reproduction, and distribution of the Work otherwise complies with
+      the conditions stated in this License.
+
+   5. Submission of Contributions. Unless You explicitly state otherwise,
+      any Contribution intentionally submitted for inclusion in the Work
+      by You to the Licensor shall be under the terms and conditions of
+      this License, without any additional terms or conditions.
+      Notwithstanding the above, nothing herein shall supersede or modify
+      the terms of any separate license agreement you may have executed
+      with Licensor regarding such Contributions.
+
+   6. Trademarks. This License does not grant permission to use the trade
+      names, trademarks, service marks, or product names of the Licensor,
+      except as required for reasonable and customary use in describing the
+      origin of the Work and reproducing the content of the NOTICE file.
+
+   7. Disclaimer of Warranty. Unless required by applicable law or
+      agreed to in writing, Licensor provides the Work (and each
+      Contributor provides its Contributions) on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+      implied, including, without limitation, any warranties or conditions
+      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+      PARTICULAR PURPOSE. You are solely responsible for determining the
+      appropriateness of using or redistributing the Work and assume any
+      risks associated with Your exercise of permissions under this License.
+
+   8. Limitation of Liability. In no event and under no legal theory,
+      whether in tort (including negligence), contract, or otherwise,
+      unless required by applicable law (such as deliberate and grossly
+      negligent acts) or agreed to in writing, shall any Contributor be
+      liable to You for damages, including any direct, indirect, special,
+      incidental, or consequential damages of any character arising as a
+      result of this License or out of the use or inability to use the
+      Work (including but not limited to damages for loss of goodwill,
+      work stoppage, computer failure or malfunction, or any and all
+      other commercial damages or losses), even if such Contributor
+      has been advised of the possibility of such damages.
+
+   9. Accepting Warranty or Additional Liability. While redistributing
+      the Work or Derivative Works thereof, You may choose to offer,
+      and charge a fee for, acceptance of support, warranty, indemnity,
+      or other liability obligations and/or rights consistent with this
+      License. However, in accepting such obligations, You may act only
+      on Your own behalf and on Your sole responsibility, not on behalf
+      of any other Contributor, and only if You agree to indemnify,
+      defend, and hold each Contributor harmless for any liability
+      incurred by, or claims asserted against, such Contributor by reason
+      of your accepting any such warranty or additional liability.
+
+   END OF TERMS AND CONDITIONS
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright [2017] [Selvarajah N.]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+//------------------------------------------------------------------------------------------//
+// Author   :               Selvarajah N.                                                   //
+// Title    :               4x16 LCD library                                                //
+// File name:               lcd.h                                                           //
+// Compiler :               GNU-C, Xilinx SDK resp. Eclipse                                 //
+// Target   :               MicroBlaze with PL-Bus resp. PLB                                //
+// Created  :               08.02.17                                                        //
+// Changed  :               17.12.21                                                        //
+// Version  :               1.0                                                             //
+// Summary  :               LCD library to communicate and print with resp. on it           //
+// Revision :               - Type safety for GNUC included in order to make the code safe, //
+//                            when using lcd_print ().                                      //
+//                          - Uncommented type-safety check, cause the GNU-C compiler of    //
+//                            old Xilinx SDK uses the old C89-Standard.                     //
+//                          - #defines are commented out and replaced by enums.             //
+//------------------------------------------------------------------------------------------//
+
+/******************************* Program Notes ******************************
+ *  This program uses an 8-bit interface to communicate with the display.
+ *  The busy-flag isn't checked, hence, the timing delays are longer
+ *  than the specified minimum of the respective data-sheet.
+ ****************************************************************************
+ *
+ ********************************** Pinout **********************************
+ *
+ *           ----------------                   ----------------
+ *          |   MicroBlaze   |                 |       LCD      |
+ *          |                |                 |                |
+ *          |  GPIO_LCD (11) |---------------->| D7             |
+ *          |  GPIO_LCD (10) |---------------->| D6             |
+ *          |  GPIO_LCD (09) |---------------->| D5             |
+ *          |  GPIO_LCD (08) |---------------->| D4             |
+ *          |  GPIO_LCD (08) |---------------->| D3             |
+ *          |  GPIO_LCD (07) |---------------->| D2             |
+ *          |  GPIO_LCD (06) |---------------->| D1             |
+ *          |  GPIO_LCD (05) |---------------->| D0             |
+ *          |                |                 |                |
+ *          |  GPIO_LCD (04) |---------------->| RS             |
+ *          |  GPIO_LCD (03) |---------------->| RW             |
+ *          |  GPIO_LCD (02) |---------------->| EN             |
+ *           ----------------                   ----------------
+ *
+ *********************************** End ***********************************/
+
+//******************************************************************
+// Include guard
+//******************************************************************
+
+#ifndef LCD_H_
+#define LCD_H_
+
+//#ifdef __GNUC__                                       // Check if it's a GNU C compiler.
+//          __attribute__ ((format (printf, 1, 2)))     // Tell the compiler that a variadic function exists and it should be treated like printf (). Definition follows afterwards.
+//#endif
+
+//******************************************************************
+// Includes
+//******************************************************************
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "platform.h"
+#include "microblaze_sleep.h"
+#include "xgpio.h"
+#include "xparameters.h"
+#include "xil_cache.h"
+#include "gpio.h"
+#include "typedefinitions.h"
+
+//******************************************************************
+// Defines
+//******************************************************************
+/*
+#define LCD_Clear           0x01        // Replaces all characters with ASCII 'space'.
+#define LCD_Home            0x02        // Returns the cursor to first position on first line.
+#define LCD_EntryMode       0x06        // Shifts cursor from left to right on read/write.
+#define LCD_DisplayOff      0x08        // Turns display off.
+#define LCD_DisplayOn       0x0C        // Display on, cursor off, don't blink character.
+#define LCD_FunctionSet     0x38        // Sets the data-width at 8-bit, 2-line resp. 4-line and 5x10 dots.
+#define LCD_FunctionReset   0x30        // Resets the LCD.
+#define LCD_CursorShowBlink 0x03        // Show cursor and blink for display/off control.
+#define LCD_SetCursor       0x80        // Sets cursor position.
+*/
+
+//******************************************************************
+// LCD Commands
+//******************************************************************
+typedef enum LCD_Commands {
+  LCD_Clear = 0x01,
+  LCD_Home = 0x02,
+  LCD_EntryMode = 0x06,
+  LCD_DisplayOff = 0x08,
+  LCD_DisplayOn = 0x0C,
+  LCD_FunctionSet = 0x38,
+  LCD_FunctionReset = 0x30,
+  LCD_CursorShowBlink = 0x03,
+  LCD_SetCursor = 0x80
+} lcd_command_t;
+
+//******************************************************************
+// Function prototypes
+//******************************************************************
+// 1: Sets display direction from left-to-right and cursor placement in incremental way.
+// 2: Clears the LCD.
+// 3: Puts the command on the data path of LCD.
+// 4: Writes out a string one by one as an ASCII code.
+// 5: Same like 9, additionally it supports cursor placement.
+// 6: Prints one char in ASCII format.
+// 7: Same like 9, additionally it supports format-string (e.g. %i) like printf.
+// 8: Sends command to LCD where the next char will be printed.
+// 9: Converts special character if there is one and returns the necessary code.
+// 10: Reverts an 8-bit int number.
+// 11: Reverts a string resp. array.
+// 12: Converts integer to string.
+// 13: Prints three zeroes if the coordinate value is zero.
+// 14: Converts a decimal number to hex in ASCII code.
+//*******************************************************************
+
+void lcd_init               (void);
+void lcd_clear              (void);
+void lcd_command            (lcd_command_t command);
+void lcd_write              (charptr_t string);
+void lcd_write_xy           (charptr_t string, int row, int column);
+void lcd_write_char         (int ascii);
+void lcd_print              (charptr_t string,...); //__attribute__ ((format (printf, 1, 2)));      // Tell the compiler to treat the function like printf (), so the compiler can look for type safety likewise with printf ().
+void cursor                 (int row, int column);
+char special_character      (char character);
+int  reverse_char           (int x);
+void reverse                (char s[]);
+void itoa                   (int n, char s[]);
+void significant_zero_place (float number);
+charptr_t hex_to_ascii      (uint32_t hex);
+
+#endif /* LCD_H_ */
